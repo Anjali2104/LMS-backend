f333956adf337006fbc47b5954af391b48ecf3e4
Anjali2104 <anjaliu949@gmail.com>
2023-10-02 17:21:40 +0530

    added createCourse methods

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,7 @@
 import Course from "../models/courseModel.js"
 import AppError from "../utils/appError.js"
+import cloudinary from 'cloudinary'
+import fs from 'fs/promises'
 
 export const getAllCourses = async (req,res,next) => {
     try {
@@ -38,4 +40,74 @@ export const getLecturesByCourseId =  async(req,res,next) => {
         )
     }
    
+}
+
+export const createCourse = async(req,res,next) => {
+
+ try {
+    const { title, description, category, createdBy } = req.body;
+    if(!title || !description || !category || !createdBy){
+        return next(
+            new AppError('All fields are required', 400)
+        )
+    } 
+     
+    const course = await Course.create({
+        title,
+        description,
+        category,
+        createdBy,
+        thumbnail:{
+            public_id:"DUMMY_DATA",
+            secure_url:"DUMMY_URL"
+        },
+    });
+
+    if(req.file){
+      const result = await cloudinary.v2.uploader.upload(req.file.path, {
+        folder:'lms',
+      });
+      if(result){
+        course.thumbnail.public_id = result.public_id;
+        course.thumbnail.secure_url = result.secure_url;
+      }
+
+      fs.rm(`uploads/${req.file.filename}`);
+
+    }
+
+    await course.save();
+
+    res.status(200).json({
+        success:true,
+        message:'Course created successfully!',
+        course,
+    })
+
+
+ } catch (error) {
+    return next(
+        new AppError(error.message, 500)
+    )
+ }
+}
+
+export const updateCourse = async(req,res,next) => {
+  try {
+    
+  } catch (error) {
+    return next(
+        new AppError(error.message, 500)
+    )
+  }
+}
+
+export const deleteCourse = async(req,res,next) => {
+  try {
+    
+  } catch (error) {
+    return next(
+        new AppError(error.message, 500)
+    )
+  }
 }
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,14 +1,27 @@
 import { Router } from "express";
-import { getAllCourses, getLecturesByCourseId } from "../controllers/courseController.js";
+import { createCourse, 
+    deleteCourse, 
+    getAllCourses,
+    getLecturesByCourseId, 
+    updateCourse } 
+    from "../controllers/courseController.js";
 import { isLoggedIn } from "../middlewares/authMiddleware.js";
+import upload from "../middlewares/multerMiddleware.js";
 
 const router = Router();
 
 router
     .route('/')
     .get(getAllCourses)
+    .post(
+        upload.single('thumbnail'),
+        createCourse
+    );
+
 router
     .route('/:courseId')
     .get( isLoggedIn, getLecturesByCourseId)
+    .put(updateCourse)
+    .delete(deleteCourse);
 
 export default router;
